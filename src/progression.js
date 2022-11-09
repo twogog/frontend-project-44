0f84071164fe791cabc2dd046ceb7092b66633ec
twogog <twogogtwogog@gmail.com>
2022-11-09 13:12:36 +0600

    made the last game

---
+++ src/progression.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import commonLogic from './index.js';
+
+const gameLogic = () => {
+  const [progrLength, progrStep] = [_.random(5, 10), _.random(1, 10), _.random(0, 100)];
+  let progFirst = _.random(0, 100);
+  const progDots = _.random(0, progrLength);
+  const progression = [];
+  for (let i = 0; i < progrLength; i += 1) {
+    progression.push(` ${progFirst}`);
+    progFirst += progrStep;
+    if (progression.length === progDots) { progression[i] = '..'; }
+  }
+  console.log(`Question: ${progression}`);
+  return progrStep;
+};
+
+export default () => {
+  commonLogic('What number is missing in the progression.', gameLogic, 'number');
+};
