ca80b79d0b0f8d5b1012caf5ab8d60e0f8dc2656
twogog <twogogtwogog@gmail.com>
2022-11-09 14:01:05 +0600

    made another last game:D

---
+++ src/prime.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import commonLogic from './index.js';
+
+const gameLogic = () => {
+  const number = _.random(0, 1000);
+  console.log(`Question: ${number}`);
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+export default () => {
+  commonLogic('Answer "yes" if given number is prime, otherwise answer "no".', gameLogic, 'string');
+};
